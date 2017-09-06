13b3e3fa2d3a3d5a849a08a1c548287eaaf0e0c6
Johan Leino <johan.leino@aptitud.se>
2017-09-06 08:03:48 +0200

    Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
                 name: "posts",
                 path: `${__dirname}/src/posts`
             }
-        }
+        },
+        "gatsby-transformer-remark"
     ]
 };
